1042c44d22276db0ca95a40a3af206de6f87e754
shijun Wei <191796158@qq.com>
2019-03-09 17:00:51 +0800

    Update cors.js

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   return function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8090');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS,POST,DELETE');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Max-Age', 60);
